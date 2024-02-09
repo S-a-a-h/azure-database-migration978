b8df388da5c69b6b4d0098995c328c2798f7f385
S-a-a-h <sam_jb_2@hotmail.co.uk>
2024-02-09 18:50:22 +0000

    adds query files

---
+++ migration_validation_queries.sql
@@ -0,0 +1,22 @@
+SELECT * FROM HumanResources.Employee;
+
+SELECT * FROM Person.Person;
+
+SELECT name AS TableName
+FROM sys.tables
+ORDER BY name;
+
+--there is an extra table:
+SELECT * FROM __migration_status;
+
+EXEC sp_help 'Person.ContactType';
+
+SELECT
+    MAX(ModifiedDate) AS MaxValue,
+    MIN(ModifiedDate) AS MinValue
+FROM Person.ContactType;
+
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'HumanResources'
+    AND TABLE_NAME = 'Employee';
